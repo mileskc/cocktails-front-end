8c78a0a2886077a3d743bba64505935ad4f74392
Ben Burke <benjaminstevenburke@gmail.com>
2019-08-26 09:14:20 -0400

    deleted edit in modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -9,7 +9,7 @@ const modal = (props) => {
                     opacity: props.show ? '1' : '0'
                 }}>
                 <div className="modal-header">
-                    <h3>Edit</h3>
+                   
                     <span className="close-modal-btn" onClick={props.close}>×</span>
                 </div>
                 <div className="modal-body">
