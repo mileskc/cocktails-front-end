ffddf279b79c2313dec6d04ac6989d39b029dba5
Katelyn <katelyncmiles@gmail.com>
2020-01-20 21:01:35 -0500

    fixed baseurl

--- src/components/NewForm.js
+++ src/components/NewForm.js
@@ -7,7 +7,7 @@ let baseURL = process.env.REACT_APP_BASEURL;
 if(process.env.NODE_ENV === 'development') {
     baseURL = 'http://localhost:3003';
 } else {
-    baseURL = 'https://fathomless-sierra-68956.herokuapp.com';
+    baseURL = 'https://cryptic-chamber-65846.herokuapp.com';
 }
 
     class NewForm extends Component {
