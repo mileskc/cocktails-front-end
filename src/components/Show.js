a5e8ff6d455c2531cabf5df0bbf9d4f423ca75dc
Katelyn <katelyncmiles@gmail.com>
2020-01-21 10:37:06 -0500

    fixed home links on modal

--- src/components/Show.js
+++ src/components/Show.js
@@ -4,7 +4,8 @@ class Show extends React.Component {
     render() {
         return (
             <div className="details">
-                <h3>Cocktail Info</h3> <a href='/'>home</a>
+                <a className = "xLink" href='/'>X</a>
+                <h3>Cocktail Info</h3> 
                 {/* <button onClick={() => this.props.hideShowCard()}>Hide Info</button> */}
                 <hr/>
                 <h4> {this.props.cocktail.name}</h4>
@@ -16,13 +17,13 @@ class Show extends React.Component {
 
                 {this.props.cocktail.favorite === true ?
                     <div class="buttonWidth">
-                        <button className="waves-effect waves-light btn" onClick={() => this.props.revealFavorite(this.props.cocktail)}>Favorite/Unfavorite</button>
+                        <button className="waves-effect waves-light btn" onClick={() => this.props.revealFavorite(this.props.cocktail)}>Star/Unstar</button>
                     </div>
                     : null
                 }
                 {this.props.cocktail.favorite === false ?
                     <div class="buttonWidth">
-                        <button className="waves-effect waves-light btn" onClick={() => this.props.revealFavorite(this.props.cocktail)}>Favorite/Unfavorite</button>
+                        <button className="waves-effect waves-light btn" onClick={() => this.props.revealFavorite(this.props.cocktail)}>Star/Unstar</button>
                     </div>
                     : null
                 }
@@ -50,7 +51,7 @@ class Show extends React.Component {
                 </ul>
                 <h5>Instructions:</h5>
                 <p className = "instructions">{this.props.cocktail.instructions}</p>
-                <a href='/'>home</a>
+                <a className = "homeLink" href='/'> ← Home</a>
             </div>
             
         )
